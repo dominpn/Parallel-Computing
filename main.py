4ebe818d686a7ce519eb63b57c227e3fe27b39dc
dominpn <dominik.krystkowiak@gmail.com>
2019-05-26 19:49:59 +0200

    Live recording

--- main.py
+++ main.py
@@ -20,12 +20,20 @@ def main():
         frames_per_buffer=CHUNK
     )
 
-    data = stream.read(CHUNK)
-    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
-
     fig, ax = plt.subplots()
-    ax.plot(data_int, '-')
-    plt.show()
+
+    x = np.arange(0, 2 * CHUNK, 2)
+    line, = ax.plot(x, np.random.rand(CHUNK))
+    ax.set_ylim(0, 255)
+    ax.set_xlim(0, CHUNK)
+
+    while True:
+        data = stream.read(CHUNK)
+        data_int = np.array(struct.unpack(str(2 * CHUNK) + 'B', data), dtype='b')[::2] + 128
+        line.set_ydata(data_int)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        plt.pause(0.000000000000001)
 
 
 if __name__ == '__main__':
